a375af471d9459dac7367ed26d663ed398d2b57f
Henrique Valcanaia <henrique@guidebook.com>
2019-03-15 18:13:36 -0300

    Initial Commit

---
+++ AttributedStringWrapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttributedStringWrapper
+//
+//  Created by Henrique Valcanaia on 15/03/19.
+//  Copyright © 2019 Henrique Valcanaia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
