4d0c9f8aba6b1dec1eadeff76412f4a691aa6370
Henrique Valcanaia <henrique@guidebook.com>
2019-03-17 17:52:01 -0300

    gitignore

---
+++ AttributedStringWrapper/NSAttributedString+StringAttribute.swift
@@ -0,0 +1,9 @@
+//
+//  NSAttributedString+StringAttribute.swift
+//  AttributedStringWrapper
+//
+//  Created by Henrique Valcanaia on 15/03/19.
+//  Copyright © 2019 Henrique Valcanaia. All rights reserved.
+//
+
+import Foundation
---
+++ AttributedStringWrapper/StringAttribute.swift
@@ -0,0 +1,9 @@
+//
+//  StringAttribute.swift
+//  AttributedStringWrapper
+//
+//  Created by Henrique Valcanaia on 15/03/19.
+//  Copyright © 2019 Henrique Valcanaia. All rights reserved.
+//
+
+import Foundation
